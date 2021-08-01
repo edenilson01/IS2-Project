7ec7b28aa943303dce8ec6939f2fdef4806cb755
Edenilson Dominguez <edominguez@es.com.py>
2021-08-01 18:06:03 -0400

    Mensajes de error modificados para usar de forma general

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,9 +1,9 @@
 #aqui van los controladores, encargados de la logica del negocio
 #se pueden agregar mas archivos
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.template import Template, Context, loader
+from django.template import loader
 
 from sys import path as path
 path.append('./')
@@ -11,7 +11,8 @@ from app.models.user_model import UserModel
 
 class ViewRequest:
     def __init__(self):
-        pass
+        self.mensaje_error = []
+        self.titulo_error = None
 
     def iniciar_sesion(self, request):
         return render(request, 'login.html')
@@ -26,16 +27,25 @@ class ViewRequest:
 
         base_password = modelo.consult_password(usuario)
 
-        respuesta = 0
         if base_password is not None:
             if base_password == psw:
-                return render(request, 'redireccion.html')
+                return redirect('/home/')
 
-        view = loader.get_template('login.html')
+        #sino redirecciona, entonces es un error de credenciales
+        self.titulo_error = 'Credenciales Inválidas'
+        self.mensaje_error.append('El nombre de usuario y/o la contraseña que ingresaste no coinciden con nuestros registros.')
+        self.mensaje_error.append('Por favor, revisa e inténtalo de nuevo.')
 
-        html = view.render({'respuesta': respuesta})
-
-        return HttpResponse(html)
+        return self.enviar_sms_error()
     
+    def enviar_sms_error(self):        
+        view = loader.get_template('login.html')
+        html_reponse = view.render({'titulo': self.titulo_error, 'errors': self.mensaje_error})
+
+        self.mensaje_error = []
+        return HttpResponse(html_reponse)
+        
+ 
+
     def home(self, request):
         return render(request, 'home.html')
